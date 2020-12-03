2e97dd9f8f863d7dcc5de821edf6443437021ab2
Nyash-Mauro <mauricee423@gmail.com>
2020-12-03 12:02:42 +0300

    added navbar components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // import { HomeComponent } from './components/home/home.component';
-// import { LoginComponent } from './components/login/login.component';
-// import { AuthGuard } from './guards/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
  const routes: Routes = [  
   //  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-// { path: 'login', component: LoginComponent },
+{ path: 'login', component: LoginComponent },
 // { path: 'post', component: PostComponent, canActivate: [AuthGuard] },
 // { path: 'user/:id', component: UserProfileComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { ToastrModule } from 'ngx-toastr';
+// import { ToastrModule } from 'ngx-toaster';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { HighlightDirective } from './directives/hightlight.directive';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HighlightDirective
+    HighlightDirective,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
